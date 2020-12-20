ad503b81993e4a04f0e3794161ea1e702594ed09
Brian Lukonsolo <brian.lukonsolo@gmail.com>
2020-12-20 20:11:28 +0000

    feat(*): add new class and tests to format base 64 string

---
+++ src/main/java/FormatInto64CharacterLines.java
@@ -0,0 +1,21 @@
+public class FormatInto64CharacterLines {
+    public String format(int numberOfCharsPerLine, String loadedX509CertBas64String) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int marker = 0;
+
+        for(char c : loadedX509CertBas64String.toCharArray()){
+            final String newLine = System.getProperty("line.separator");
+            if(marker == numberOfCharsPerLine){
+               stringBuilder.append(newLine);
+               marker = 0;
+            }
+            stringBuilder.append(c);
+            marker++;
+
+        }
+
+        return stringBuilder.toString();
+
+    }
+
+}
---
+++ src/test/java/FormatInto64CharacterLinesTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class FormatInto64CharacterLinesTest {
+    final String formattedCertificateStringFile = "src/test/resources/formattedCertificateBase64String";
+    final String certificateStringFile = "src/test/resources/dummyX509CertificateString";
+
+    @Test
+    public void itShouldFormatAnyStringInto64CharacterLines() throws IOException {
+        String expected = new LoadCertificateBase64String().loadStringFromFile(formattedCertificateStringFile);
+        String loadedX509CertBas64String = new LoadCertificateBase64String().loadStringFromFile(certificateStringFile);
+        String actual = new FormatInto64CharacterLines()
+                .format(64, loadedX509CertBas64String);
+        assertThat("certificate must be formatted into 64 char lines", actual, is(equalTo(expected)));
+    }
+}
