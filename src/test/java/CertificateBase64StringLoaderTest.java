2e152d0b4cefa366d65feb05212fa94d53d67ba7
Brian Lukonsolo <brian.lukonsolo@gmail.com>
2020-12-20 17:56:17 +0000

    feat(*): change certificate text to valid certificate string

--- src/test/java/CertificateBase64StringLoaderTest.java
+++ src/test/java/CertificateBase64StringLoaderTest.java
@@ -9,7 +9,7 @@ public class CertificateBase64StringLoaderTest {
     final String certificateStringFile = "src/test/resources/dummyX509CertificateString";
     @Test
     public void itShouldLoadCertificateBase64StringFromAFileWithoutModifyingTheContent() throws IOException {
-        String expectedString = "abcde";
+        String expectedString = "MIICajCCAdOgAwIBAgIBADANBgkqhkiG9w0BAQ0FADBSMQswCQYDVQQGEwJ1czETMBEGA1UECAwKQ2FsaWZvcm5pYTEVMBMGA1UECgwMT25lbG9naW4gSW5jMRcwFQYDVQQDDA5zcC5leGFtcGxlLmNvbTAeFw0xNDA3MTcxNDEyNTZaFw0xNTA3MTcxNDEyNTZaMFIxCzAJBgNVBAYTAnVzMRMwEQYDVQQIDApDYWxpZm9ybmlhMRUwEwYDVQQKDAxPbmVsb2dpbiBJbmMxFzAVBgNVBAMMDnNwLmV4YW1wbGUuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDZx+ON4IUoIWxgukTb1tOiX3bMYzYQiwWPUNMp+Fq82xoNogso2bykZG0yiJm5o8zv/sd6pGouayMgkx/2FSOdc36T0jGbCHuRSbtia0PEzNIRtmViMrt3AeoWBidRXmZsxCNLwgIV6dn2WpuE5Az0bHgpZnQxTKFek0BMKU/d8wIDAQABo1AwTjAdBgNVHQ4EFgQUGHxYqZYyX7cTxKVODVgZwSTdCnwwHwYDVR0jBBgwFoAUGHxYqZYyX7cTxKVODVgZwSTdCnwwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQ0FAAOBgQByFOl+hMFICbd3DJfnp2Rgd/dqttsZG/tyhILWvErbio/DEe98mXpowhTkC04ENprOyXi7ZbUqiicF89uAGyt1oqgTUCD1VsLahqIcmrzgumNyTwLGWo17WDAa1/usDhetWAMhgzF/Cnf5ek0nK00m0YZGyc4LzgD0CROMASTWNg==";
         String loadedX509CertBas64String = new LoadCertificateBase64String().loadStringFromFile(certificateStringFile);
 
         assertThat("String is loaded", loadedX509CertBas64String, is(equalTo(expectedString)));
